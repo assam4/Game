32764837cce017024e4a4d5f3108596d2ae371aa
Samvel <samaslanyan3@gmail.com>
2023-11-23 22:33:51 +0400

    Add project

---
+++ Game_project/game.cpp
@@ -0,0 +1,86 @@
+#include "freader.hpp"
+#include <fstream>
+#include <vector>
+using namespace std;
+
+static vector<string> questions;
+static vector<string> answer1;
+static vector<string> answer2;
+
+string answer_shadow(const string& answer)
+{
+	if (answer[0] == '*')
+	{
+	
+		string temp;
+		for (int i = 1; i < answer.size(); ++i)
+			temp += answer[i];
+		return temp;
+
+	}
+	return answer;
+}
+
+void game()
+{
+	ifstream is("Questions.txt");
+	if (!is.is_open()) throw "Cannot loading game...Try latter";
+	Inputstream read(is);
+	try {
+		while (!read.eofbit())
+		{
+
+			questions.push_back(read.readfromJSON());
+			answer1.push_back(read.readfromJSON());
+			answer2.push_back(read.readfromJSON());
+
+		}
+	}
+	catch (...) { }
+
+	if (questions.empty()) throw "No data...";
+
+	for (int index = 0; index < questions.size(); ++index)
+	{
+		cout << questions[index] << endl;
+		cout << "[1] " << answer_shadow(answer1[index]) << " [2] " << answer_shadow(answer2[index]) << endl;
+		int choice;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			if (answer1[index][0] == '*')
+			{
+				cout << "Good." << endl;
+				break;
+			}
+			else throw "Wrong choice... Game Over !";
+		case 2:
+			if (answer2[index][0] == '*')
+			{
+				cout << "Good." << endl;
+				break;
+			}
+			else throw "Wrong choice... Game Over !";
+		default:
+			throw "Wrong choice... Game Over !";
+
+		};
+
+
+
+	}
+}
+
+	int main()
+	{
+		try
+		{
+			game();
+		}
+		catch (const char* s)
+		{
+			cerr << s << endl;
+		}
+		return 0;
+	}
